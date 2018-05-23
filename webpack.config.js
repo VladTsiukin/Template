e44a282c288b92f57c4fb9043c76f59206746c46
vladislav <32671668+VladTsiukin@users.noreply.github.com>
2018-05-23 12:47:06 +0300

    add html-webpack-plugin and clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const devMode = process.env.NODE_ENV !== 'production';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: { 
@@ -16,12 +18,19 @@ module.exports = {
     filename: 'main.js'
   },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
       filename: devMode ? '[name].css' : '[name].[hash].css',
       chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
-    })
+    }),
+    new HtmlWebpackPlugin({
+        inject: false,
+        hash: true,
+        template: './src/index.html',
+        filename: 'index.html'
+      })
   ],
   module: {
     rules: [
