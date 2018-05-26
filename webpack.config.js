39c5eaf5cba9f531358bb82bd975cda998739e15
vladislav <32671668+VladTsiukin@users.noreply.github.com>
2018-05-26 18:52:50 +0300

    add devtool option

--- webpack.config.js
+++ webpack.config.js
@@ -7,38 +7,39 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: { 
-        app: [
-            './src/index.js',
-            './src/style/main.scss'
-        ]
+    //devtool: 'source-map', // Enable to debug js code
+    entry: { 
+            app: [
+                './src/index.js',
+                './src/style/main.scss'
+            ]
+        },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: devMode ? '[name].js' : '[name].[chunkhash].js',
     },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: devMode ? '[name].js' : '[name].[chunkhash].js',
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: devMode ? '[name].css' : '[name].[contenthash].css',
-      chunkFilename: devMode ? '[id].css' : '[id].[contenthash].css',
-    }),
-    new HtmlWebpackPlugin({
-        inject: true,
-        hash: true,
-        template: './src/index.html',
-        filename: 'index.html'
-      })
-  ],
-  module: {
+    plugins: [
+        new CleanWebpackPlugin(['dist']), // clean folder 'dist'
+        new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: devMode ? '[name].css' : '[name].[contenthash].css',
+        chunkFilename: devMode ? '[id].css' : '[id].[contenthash].css',
+        }),
+        new HtmlWebpackPlugin({
+            inject: true,
+            hash: true,
+            template: './src/index.html',
+            filename: 'index.html'
+        })
+    ],
+    module: {
     rules: [
         {
             enforce: "pre",
             test: /\.js$/,
             exclude: /node_modules/,
-            loader: "eslint-loader",
+            loader: "eslint-loader", // enable eslint
             options: {
                 fix: true
             }
